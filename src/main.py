4a2cfd2e5993675710c7240e5fff3e46f1cf2df5
WeiLing Pan <zz860530@gmail.com>
2024-12-01 18:10:03 +0800

    feat: add mutation for create user

--- src/main.py
+++ src/main.py
@@ -21,32 +21,45 @@ fake = Faker()
 # DB init
 Base.metadata.create_all(bind=engine)
 
-# @strawberry.type
-# class Query:
-#     @strawberry.field
-#     def user(self, id: int) -> User:
-#         try:
-#             return get_user_data(id)
-#         except ValueError as e:
-#             raise ValueError(str(e))
-
 @strawberry.type
 class Query:
     @strawberry.field
     def user(self, id: Optional[int] = None) -> List[User]:
-        if id:
-            return [User(
-                id=id, 
-                name=fake.name(), 
-                age=fake.random_int(min=18, max=80))]
-        
-        return [User(
-            id=fake.uuid4(),
-            name=fake.name(),
-            age=fake.random_int(min=18, max=80)
-        ) for _ in range(5)]
+        try:
+            return get_user_data(id)
+        except ValueError as e:
+            raise ValueError(str(e))
     
-schema = strawberry.Schema(query=Query)
+# fake data
+# @strawberry.type
+# class Query:
+#     @strawberry.field
+#     def user(self, id: Optional[int] = None) -> List[User]:
+#         if id:
+#             return [User(
+#                 id=id, 
+#                 name=fake.name(), 
+#                 age=fake.random_int(min=18, max=80))]
+        
+#         return [User(
+#             id=fake.uuid4(),
+#             name=fake.name(),
+#             age=fake.random_int(min=18, max=80)
+#         ) for _ in range(5)]
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def create_user(self, name: str, age: int) -> User:
+        db = SessionLocal()
+        user = UserModel(name=name, age=age)
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        db.close()
+        return User(id=user.id, name=user.name, age=user.age)
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 graphql_app = GraphQL(schema)
 
 app = FastAPI(title="FastAPI + GraphQL Example", version="1.0.0")
